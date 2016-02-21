c18216999351ebecadc3640b146c18e7c1273e53
vbf711 <vince@Vinces-MacBook-Air.local>
2016-02-20 19:55:51 -0500

    added white and black

--- libraries/Color.h
+++ libraries/Color.h
@@ -9,6 +9,8 @@ public:
     Color(int red, int green, int blue) : redVal(red), greenVal(green), blueVal(blue) {}
 };
 
+Color white(255, 255, 255);
+Color black(0, 0, 0);
 Color red(255, 0, 0);
 Color green(0, 255, 0);
 Color blue(0, 0, 255);
